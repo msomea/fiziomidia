951bbdf7ea421f915ee3ea737c8e71dfb4c1b4c6
msomea <msomearaphael@gmail.com>
2026-02-09 21:55:24 +0300

    fix environment variables

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -50,6 +50,9 @@ export default {
 
   // Cloudinary
   cloudinaryUrl: process.env.CLOUDINARY_URL,
+  cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+  cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
 
   // Resend (email)
   resendApiKey: process.env.RESEND_API_KEY,
