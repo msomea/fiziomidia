d6406a2697c6d9a6dd8c65471e8c4a10c0e36082
msomea <msomearaphael@gmail.com>
2026-02-11 07:13:40 +0300

    Use transparent logo for email

--- backend/src/app.js
+++ backend/src/app.js
@@ -97,7 +97,7 @@ app.use((req, res, next) => {
 /* ---------------------------------- */
 app.get("/api/logo", (req, res) => {
   res.setHeader("Cache-Control", "public, max-age=86400"); // 24h
-  res.sendFile(path.join(process.cwd(), "public", "fiziomidia_logo.png"));
+  res.sendFile(path.join(process.cwd(), "public", "fiziomidia_logo_trans.png"));
 });
 
 /* ---------------------------------- */
