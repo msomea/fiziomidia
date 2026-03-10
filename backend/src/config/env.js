bf29f3e445e5f1f01c049252e0fc1f9676b2251b
msomea <msomearaphael@gmail.com>
2026-03-10 20:24:58 +0300

    Fix Redis Version conflict

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -81,9 +81,9 @@ export const ENV = {
   PRODUCTION_CLIENT_URL: process.env.PRODUCTION_CLIENT_URL,
 
   // Redis Cloud Configuration
-  REDIS_HOST: process.env.REDIS_HOST || "localhost",
-  REDIS_PORT: process.env.REDIS_PORT || 6379,
+  REDIS_HOST: process.env.REDIS_HOST,
+  REDIS_PORT: process.env.REDIS_PORT,
   REDIS_PASSWORD: process.env.REDIS_PASSWORD,
-  REDIS_URL: process.env.REDIS_URL || "redis://localhost:6379", // Fallback URL
+  REDIS_URL: process.env.REDIS_URL,
 };
 
